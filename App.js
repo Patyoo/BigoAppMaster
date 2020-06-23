04584146fcf70ee55f949cc6dcd2065b79c0a149
Patrik <patrik.podhorsky@gmail.com>
2020-06-23 17:48:19 +0200

    Temporal Navigation

--- App.js
+++ App.js
@@ -1,9 +1,115 @@
-import React, {Component} from 'react';
-import AppContainer from './Navigation/AppNavigator';
+// import React, {Component} from 'react';
+// import AppContainer from './Navigation/AppNavigator';
 
+// export default class App extends Component {
+//   render() {
+//     return <AppContainer />;
+//   }
+// }
 
-export default class App extends Component {
-  render() {
-    return <AppContainer />;
-  }
+import 'react-native-gesture-handler';
+
+import * as React from 'react';
+import {Button, View, Text, TouchableOpacity, Image} from 'react-native';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+
+import HomeScreen from './pages/HomeScreen';
+import DetailsScreen from './pages/DetailsScreen';
+import ProfileScreen from './pages/ProfileScreen';
+import SettingsScreen from './pages/SettingsScreen';
+
+const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
+
+function HomeStack() {
+  return (
+    <Stack.Navigator
+      initialRouteName="Home"
+      screenOptions={{
+        headerStyle: {backgroundColor: '#42f44b'},
+        headerTintColor: '#fff',
+        headerTitleStyle: {fontWeight: 'bold'},
+      }}>
+      <Stack.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{title: 'Home Page'}}
+      />
+      <Stack.Screen
+        name="Details"
+        component={DetailsScreen}
+        options={{title: 'Details Page'}}
+      />
+    </Stack.Navigator>
+  );
 }
+
+function SettingsStack() {
+  return (
+    <Stack.Navigator
+      initialRouteName="Settings"
+      screenOptions={{
+        headerStyle: {backgroundColor: '#42f44b'},
+        headerTintColor: '#fff',
+        headerTitleStyle: {fontWeight: 'bold'},
+      }}>
+      <Stack.Screen
+        name="Settings"
+        component={SettingsScreen}
+        options={{title: 'Setting Page'}}
+      />
+      <Stack.Screen
+        name="Details"
+        component={DetailsScreen}
+        options={{title: 'Details Page'}}
+      />
+      <Stack.Screen
+        name="Profile"
+        component={ProfileScreen}
+        options={{title: 'Profile Page'}}
+      />
+    </Stack.Navigator>
+  );
+}
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator
+        initialRouteName="Feed"
+        tabBarOptions={{
+          activeTintColor: '#42f44b',
+        }}>
+        <Tab.Screen
+          name="HomeStack"
+          component={HomeStack}
+          options={{
+            tabBarLabel: 'Home',
+            tabBarIcon: ({color, size}) => (
+              <MaterialCommunityIcons name="home" color={color} size={size} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="SettingsStack"
+          component={SettingsStack}
+          options={{
+            tabBarLabel: 'Settings',
+            tabBarIcon: ({color, size}) => (
+              <MaterialCommunityIcons
+                name="settings"
+                color={color}
+                size={size}
+              />
+            ),
+          }}
+        />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
+export default App;
